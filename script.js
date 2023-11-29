38b434ef0ad487254a5ecea5c853edea4999b6f4
emman-pip <parrenoemmanuel756@gmail.com>
2023-11-29 17:19:54 +0800

    Tweak deisgn a little.

--- script.js
+++ script.js
@@ -9,14 +9,22 @@ const slowScroll = (element, target) => {
   };
 };
 
+// creating a prototype that appends elements to the nav bar
 const burger = () => {
   const burger = document.querySelector(".burger");
-  const nav = document.querySelector("a");
+  const nav = document.querySelector("nav");
   const home = document.createElement("p");
+  const Emmanuel = document.createElement("p");
+  const Anthony = document.createElement("p");
+  const Rachelle = document.createElement("p");
+  const Krizette = document.createElement("p");
+  const Kervy = document.createElement("p");
+  const members = [home, Emmanuel, Anthony, Rachelle, Krizette, Kervy];
+
   home.textContent = "Home";
   burger.onclick = () => {
     console.log("clicked me");
-    nav.appendChild(home);
+    members.forEach((e) => nav.append(e));
   };
 };
 
