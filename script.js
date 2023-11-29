8481f0984063fea46d9a50561d40ef082fd8bcad
emman-pip <parrenoemmanuel756@gmail.com>
2023-11-29 15:15:06 +0800

    Add deisgn and javascript.

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// alert("WAG BUKSAN, NAKAMAMATAY!!!");
+const slowScroll = (element, target) => {
+  element.onscroll = () => {
+    const scrolltoTop = document.scrollingElement.scrollTop;
+
+    const xvalue = "center";
+    const scrollFactor = 0.3;
+    const yValue = scrolltoTop * scrollFactor;
+    target.style.backgroundPosition = xvalue + " " + yValue + "px";
+  };
+};
+
+const burger = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector("a");
+  const home = document.createElement("p");
+  home.textContent = "Home";
+  burger.onclick = () => {
+    console.log("clicked me");
+    nav.appendChild(home);
+  };
+};
+
+(() => {
+  alert("WAG BUKSAN, NAKAMAMATAY!!!");
+  slowScroll(
+    document.querySelector("body"),
+    document.querySelector(".groupName")
+  );
+  burger();
+})();
