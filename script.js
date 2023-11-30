573c57b00343794cb7815a10e2e2767f364f6057
emman-pip <parrenoemmanuel756@gmail.com>
2023-11-30 08:41:37 +0800

    Finish the burger.

--- script.js
+++ script.js
@@ -13,23 +13,52 @@ const slowScroll = (element, target) => {
 const burger = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector("nav");
-  const home = document.createElement("p");
-  const Emmanuel = document.createElement("p");
-  const Anthony = document.createElement("p");
-  const Rachelle = document.createElement("p");
-  const Krizette = document.createElement("p");
-  const Kervy = document.createElement("p");
+  const home = document.createElement("a");
+  const Emmanuel = document.createElement("a");
+  const Anthony = document.createElement("a");
+  const Rachelle = document.createElement("a");
+  const Krizette = document.createElement("a");
+  const Kervy = document.createElement("a");
+  const names = [
+    "Home",
+    "Emmanuel",
+    "Anthony",
+    "Rachelle",
+    "Krizette",
+    "Kervin",
+  ];
   const members = [home, Emmanuel, Anthony, Rachelle, Krizette, Kervy];
-
-  home.textContent = "Home";
+  const links = [
+    "./index.html",
+    "./emmanuel.html",
+    "./anthony.html",
+    "./rach.html",
+    "./kriz.html",
+    "./kervin.html",
+  ];
+  let clicks = 0;
   burger.onclick = () => {
-    console.log("clicked me");
-    members.forEach((e) => nav.append(e));
+    clicks++;
+    if (clicks % 2 === 1) {
+      console.log("clicked me");
+      for (let i = 0; i < members.length; i++) {
+        members[i].href = links[i];
+        const memberName = names[i];
+        members[i].textContent = memberName;
+        nav.append(members[i]);
+        nav.style = "gap:1rem;";
+      }
+      return;
+    }
+    for (let i = 0; i < members.length; i++) {
+      nav.removeChild(members[i]);
+      nav.style = "gap:0;";
+    }
   };
 };
 
 (() => {
-  alert("WAG BUKSAN, NAKAMAMATAY!!!");
+  console.log("WAG BUKSAN, NAKAMAMATAY!!!");
   slowScroll(
     document.querySelector("body"),
     document.querySelector(".groupName")
